1cd413f1c82ce8a526ba1ebe963a228fcc1ab122
endri prifti <endri@msn.com>
2013-05-29 08:12:30 +0200

    Create business_day_closing.php

---
+++ admin/includes/languages/english/business_day_closing.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  Credins Bank E-Commerce Solutions
+  http://www.bankacredins.com
+
+  Copyright (c) 2013 Endri Prifti
+
+  Credinz Payment Module for osCommerce 2.3
+*/
+
+define('HEADING_TITLE',  		'Business Day Closing');
+
+define('CLOSE_DATE',			'Date of Closure');
+define('RESULT',				'Result');
+define('RESULT_CODE',			'Result Code');
+define('COUNT_REVERSAL',		'Number of Transaction Reversals');
+define('COUNT_TRANSACTION',		'Number of Transaction');
+define('AMOUNT_REVERSAL',		'Total Amount of Transaction Reversals');
+define('AMOUNT_TRANSACTION',	'Total Amount of Transactions');
+
+?>
